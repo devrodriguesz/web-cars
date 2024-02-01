d80deedec49b8c542e4d92c8cad7c3bcde36beec
devrodriguesz <dev.emersonrodrigues@outlook.com>
2024-02-01 16:07:08 -0300

    feat: listing items on the site home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,70 @@
+import { useEffect, useState } from "react";
 import { Container } from "../../components/container";
+import { Link } from "react-router-dom";
+import {
+  collection,
+  query,
+  orderBy,
+  getDocs,
+} from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
+
+interface CarsProps {
+  id: string;
+  name: string;
+  year: string;
+  uid: string;
+  price: string | number;
+  city: string;
+  km: string;
+  images: CarImageProps[];
+}
+
+interface CarImageProps {
+  name: string;
+  uid: string;
+  url: string;
+}
 
 export function Home() {
+  const [cars, setCars] = useState<CarsProps[]>([])
+  const [loadImages, setLoadImages] = useState<string[]>([])
+
+  useEffect(() => {
+    function loadCars() {
+      const carsRef = collection(db, "cars")
+      const queryRef = query(carsRef, orderBy("created", "desc"))
+
+      getDocs(queryRef)
+        .then((snapshot) => {
+          let listCars = [] as CarsProps[];
+
+          snapshot.forEach(doc => {
+            listCars.push({
+              id: doc.id,
+              name: doc.data().name,
+              year: doc.data().year,
+              km: doc.data().km,
+              city: doc.data().city,
+              price: doc.data().price,
+              images: doc.data().images,
+              uid: doc.data().uid,
+            })
+          })
+
+          setCars(listCars)
+        })
+
+    }
+
+    loadCars();
+
+  }, [])
+
+  function handleImageLoad(id: string){
+    setLoadImages((prevImageLoaded) => [...prevImageLoaded, id])
+  }
+
   return (
     <Container>
       <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
@@ -20,28 +84,37 @@ export function Home() {
       </h1>
 
       <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <section className="w-full bg-white rounded-lg">
-          <img
-            className="w-full rounded-lg mb-2 max-h-62 hover:scale-105 transition-all"
-            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2024/202401/20240125/honda-civic-2.0-16v-flexone-ex-4p-cvt-wmimagem16330843376.jpg?s=fill&w=552&h=414&q=60"
-            alt="carro"
-          />
-          <p className="font-bold mt-1 mb-2 px-2">Honda Civic</p>
 
-          <div className="flex flex-col px-2">
-            <span className="text-zinc-700 mb-6">Ano 2016/2016 | 23.000 km</span>
-            <strong className="text-black font-medium text-xl">R$ 190.000</strong>
-          </div>
+        {cars.map(car => (
+          <Link key={car.id} to={`/car-details/${car.id}`}>
+            <section className="w-full bg-white rounded-lg">
+            <div 
+              className="w-full h-72 max-h-72 rounded-lg bg-slate-200"
+              style={{ display: loadImages.includes(car.id) ? "none" : "block"}}
+              >  
+            </div>
+              <img
+                className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+                src={car.images[0].url}
+                alt="carro"
+                onLoad={ () => handleImageLoad(car.id) }
+                style={{ display: loadImages.includes(car.id) ? "block" : "none" }}
+              />
+              <p className="font-bold mt-1 mb-2 px-2">{car.name}</p>
+              <div className="flex flex-col px-2">
+                <span className="text-zinc-700 mb-6">Ano {car.year} | {car.km} km</span>
+                <strong className="text-black font-medium text-xl">R$ {car.price} </strong>
+              </div>
+              <div className="w-full h-px bg-slate-200 my-2"></div>
+              <div className="px-2 pb-2">
+                <span className="text-zinc-700">
+                  {car.city}
+                </span>
+              </div>
+            </section>
+          </Link>
+        ))}
 
-          <div className="w-full h-px bg-slate-200 my-2"></div>
-
-          <div className="px-2 pb-2">
-            <span className="text-zinc-700">
-              Campo Grande - MS
-            </span>
-          </div>
-
-        </section>
       </main>
     </Container>
   )
