e3c1799db6be793e4df34fd87f839962323837d1
devrodriguesz <dev.emersonrodrigues@outlook.com>
2024-02-01 14:55:32 -0300

    feat: send car item for database with images

--- src/pages/dashboard/new-car/index.tsx
+++ src/pages/dashboard/new-car/index.tsx
@@ -8,13 +8,14 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { AuthContext } from "../../../contexts/AuthContext"
 import { v4 as uuidV4 } from "uuid"
 
-import { storage } from "../../../services/firebaseConnection"
+import { storage, db } from "../../../services/firebaseConnection"
 import {
   ref,
   uploadBytes,
   getDownloadURL,
   deleteObject,
 } from 'firebase/storage'
+import { addDoc, collection } from "firebase/firestore"
 
 const schema = z.object({
   name: z.string().min(1, "O campo nome é obrigatório"),
@@ -91,19 +92,54 @@ export function NewCar() {
   }
 
   function onSubmit(data: FormData) {
-    console.log(data)
+    if (carImages.length === 0) {
+      alert('Insira alguma imagem do veículo')
+      return;
+    }
+
+    const carListImages = carImages.map(car => {
+      return {
+        uid: car.uid,
+        name: car.name,
+        url: car.url,
+      }
+    })
+
+    addDoc(collection(db, "cars"), {
+      name: data.name,
+      model: data.model,
+      whatsapp: data.whatsapp,
+      city: data.city,
+      year: data.year,
+      km: data.km,
+      price: data.price,
+      description: data.description,
+      created: new Date(),
+      owner: user?.name,
+      uid: user?.uid,
+      images: carListImages,
+    })
+      .then(() => {
+        reset();
+        setCarImages([]);
+        console.log("CADASTRADO COM SUCESSO")
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
   }
 
-  async function handleDeleteImage(item: ImageItemProps){
+  async function handleDeleteImage(item: ImageItemProps) {
     const imagePath = `images/${item.uid}/${item.name}`;
 
     const imageRef = ref(storage, imagePath)
 
-    try{
+    try {
       await deleteObject(imageRef)
-      setCarImages(carImages.filter((car)=> car.url !== item.url))
+      setCarImages(carImages.filter((car) => car.url !== item.url))
     }
-    catch(err){
+    catch (err) {
       console.log('ERRO AO DELETAR')
     }
   }
@@ -131,9 +167,9 @@ export function NewCar() {
 
         {carImages.map(item => (
           <div key={item.name} className="w-full h-32 flex items-center justify-center relative">
-            <button 
-            className="absolute"
-            onClick={() => handleDeleteImage(item)}
+            <button
+              className="absolute top-2 right-3"
+              onClick={() => handleDeleteImage(item)}
             >
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#FFF" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
