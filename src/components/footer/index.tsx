179ec0c4e721d2a473aebe1eb72387e28125f265
devrodriguesz <dev.emersonrodrigues@outlook.com>
2024-02-01 23:24:30 -0300

    feat: create footer component

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,36 @@
+export function Footer() {
+    return (
+        <div className="bg-gray-900">
+            <div className="max-w-2xl mx-auto text-white py-10">
+                <div className="text-center">
+                    <h3 className="text-3xl mb-3">Baixe nosso aplicativo </h3>
+                    <p> Fique por dentro das melhores ofertas.</p>
+                    <div className="flex justify-center my-10">
+                        <div className="flex items-center border w-auto rounded-lg px-4 py-2 w-52 mx-2 cursor-pointer">
+                            <img src="https://cdn-icons-png.flaticon.com/512/888/888857.png" className="w-7 md:w-8"/>
+                                <div className="text-left ml-3">
+                                    <p className='text-xs text-gray-200'>Download on </p>
+                                    <p className="text-sm md:text-base"> Google Play Store </p>
+                                </div>
+                        </div>
+                        <div className="flex items-center border w-auto rounded-lg px-4 py-2 w-44 mx-2 cursor-pointer">
+                            <img src="https://cdn-icons-png.flaticon.com/512/888/888841.png" className="w-7 md:w-8"/>
+                                <div className="text-left ml-3">
+                                    <p className='text-xs text-gray-200'>Download on </p>
+                                    <p className="text-sm md:text-base"> Apple Store </p>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
+                    <p className="order-2 md:order-1 mt-8 md:mt-0"> &copy; WebCars, 2024. </p>
+                    <div className="order-1 md:order-2">
+                        <span className="px-2 cursor-pointer">Sobre nós</span>
+                        <span className="px-2 border-l cursor-pointer">Contato</span>
+                        <span className="px-2 border-l cursor-pointer">Política de Privacidade</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,11 +1,14 @@
 import { Header } from "../header"
 import { Outlet } from "react-router-dom"
+import { Footer } from "../../components/footer";
+
 
 export function Layout() {
     return (
         <>
             <Header />
             <Outlet />
+            <Footer />            
         </>
     )
 }
