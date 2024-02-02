3fd6ff164b1e21a4429d876c17156d47ecfbf463
devrodriguesz <dev.emersonrodrigues@outlook.com>
2024-02-01 22:02:32 -0300

    chore: dealing with future errors

--- src/pages/car-details/index.tsx
+++ src/pages/car-details/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { Container } from "../../components/container"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 
 import { getDoc, doc } from "firebase/firestore"
 import { db } from "../../services/firebaseConnection"
@@ -33,6 +33,8 @@ export function CarDetails() {
   const { id } = useParams();
   const [car, setCar] = useState<CarProps>()
   const [sliderPerView, setSliderPerView] = useState<number>(2)
+  const navigate = useNavigate()
+
 
   useEffect(() => {
     async function loadCar() {
@@ -41,6 +43,11 @@ export function CarDetails() {
       const docRef = doc(db, "cars", id)
       getDoc(docRef)
         .then((snapshot) => {
+
+          if (!snapshot.data()) {
+            navigate("/")
+          }
+
           setCar({
             id: snapshot.id,
             name: snapshot.data()?.name,
@@ -59,49 +66,51 @@ export function CarDetails() {
         })
     }
 
-    loadCar()
+    loadCar();
 
   }, [id])
 
-  useEffect(()=>{
+  useEffect(() => {
 
-    function handleResize(){
-      if(window.innerWidth < 720){
+    function handleResize() {
+      if (window.innerWidth < 720) {
         setSliderPerView(1);
       }
-      else{
+      else {
         setSliderPerView(2)
       }
     }
 
-      handleResize()
+    handleResize()
 
-      window.addEventListener("resize", handleResize)
+    window.addEventListener("resize", handleResize)
 
-      return() => {
-        window.removeEventListener("resize", handleResize)
-      }
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
 
 
-  },[])
+  }, [])
 
   return (
     <Container>
 
-      <Swiper
-        slidesPerView={sliderPerView}
-        pagination={{ clickable: true }}
-        navigation
-      >
-        {car?.images.map( image => (
-          <SwiperSlide key={image.name}>
-            <img
-              src={image.url}
-              className="w-full h-96 object-cover"
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      {car && (
+        <Swiper
+          slidesPerView={sliderPerView}
+          pagination={{ clickable: true }}
+          navigation
+        >
+          {Array.isArray(car?.images) && car?.images.map(image => (
+            <SwiperSlide key={image.name}>
+              <img
+                src={image.url}
+                className="w-full h-96 object-cover"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
 
       {car && (
         <main className="w-full bg-white rounded-lg p-6 my-4">
@@ -137,7 +146,10 @@ export function CarDetails() {
           <strong>Whatsapp</strong>
           <p className="mb-4">{car?.whatsapp}</p>
 
-          <a className="cursor-pointer bg-green-500 w-full text-white flex items-center justify-center gap-2 my-6 h-11 text-l rounded-lg ">
+          <a
+            target="_blank"
+            href={`https://api.whatsapp.com/send?phone=${car?.whatsapp}&text=Olá vi esse ${car?.name} no site WebCars e fiquei interessado`}
+            className="cursor-pointer bg-green-500 w-full text-white flex items-center justify-center gap-2 my-6 h-11 text-l rounded-lg ">
             Conversar com vendedor
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
