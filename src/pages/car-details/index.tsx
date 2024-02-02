e1c80ebe06e9c8eca43296d8cd42c9a0d81208c8
devrodriguesz <dev.emersonrodrigues@outlook.com>
2024-02-01 21:47:12 -0300

    feat: slider in the car details page

--- src/pages/car-details/index.tsx
+++ src/pages/car-details/index.tsx
@@ -5,6 +5,8 @@ import { useParams } from "react-router-dom"
 import { getDoc, doc } from "firebase/firestore"
 import { db } from "../../services/firebaseConnection"
 
+import { Swiper, SwiperSlide } from "swiper/react"
+
 interface CarProps {
   id: string;
   name: string;
@@ -30,6 +32,7 @@ interface ImagesCarProps {
 export function CarDetails() {
   const { id } = useParams();
   const [car, setCar] = useState<CarProps>()
+  const [sliderPerView, setSliderPerView] = useState<number>(2)
 
   useEffect(() => {
     async function loadCar() {
@@ -60,9 +63,45 @@ export function CarDetails() {
 
   }, [id])
 
+  useEffect(()=>{
+
+    function handleResize(){
+      if(window.innerWidth < 720){
+        setSliderPerView(1);
+      }
+      else{
+        setSliderPerView(2)
+      }
+    }
+
+      handleResize()
+
+      window.addEventListener("resize", handleResize)
+
+      return() => {
+        window.removeEventListener("resize", handleResize)
+      }
+
+
+  },[])
+
   return (
     <Container>
-      <h1>SLIDER</h1>
+
+      <Swiper
+        slidesPerView={sliderPerView}
+        pagination={{ clickable: true }}
+        navigation
+      >
+        {car?.images.map( image => (
+          <SwiperSlide key={image.name}>
+            <img
+              src={image.url}
+              className="w-full h-96 object-cover"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
       {car && (
         <main className="w-full bg-white rounded-lg p-6 my-4">
@@ -98,7 +137,7 @@ export function CarDetails() {
           <strong>Whatsapp</strong>
           <p className="mb-4">{car?.whatsapp}</p>
 
-          <a className="cursor-pointer bg-green-500 w-full text-white flex items-center justify-center gap-2 my-6 h-11 text-xl rounded-lg font-medium">
+          <a className="cursor-pointer bg-green-500 w-full text-white flex items-center justify-center gap-2 my-6 h-11 text-l rounded-lg ">
             Conversar com vendedor
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
